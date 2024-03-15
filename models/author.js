9a5cd3442d8d31978b4766a7a47e82aed9d4f29b
Shruti <52789421+shrued@users.noreply.github.com>
2024-03-15 15:58:57 -0400

    Update author.js

--- models/author.js
+++ models/author.js
@@ -28,7 +28,16 @@ AuthorSchema
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifespan = '';
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan = this.date_of_birth.getFullYear() + ' - ' + this.date_of_death.getFullYear();
+  } 
+  if (this.date_of_birth && !this.date_of_death) {
+    lifespan = this.date_of_birth.getFullYear() + ' - ';
+  }
+  return lifespan;
+});
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
